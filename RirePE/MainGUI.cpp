15a6065d059012426b2d11445175cb4d1aa10965
Riremito <29014872+Riremito@users.noreply.github.com>
2023-01-09 00:43:07 +0900

    x64

--- RirePE/MainGUI.cpp
+++ RirePE/MainGUI.cpp
@@ -1,5 +1,4 @@
 #include"../RirePE/MainGUI.h"
-
 // メモ
 void SetInfo(std::wstring wText) {
 	Alice &a = GetMainGUI();
@@ -29,8 +28,207 @@ std::wstring GetPacketType(PacketData &pd) {
 	return L"Error";
 }
 
+std::wstring GetFormatType(PacketFormat &pf) {
+	switch (pf.type) {
+	case ENCODEHEADER:
+	{
+		return L"HEADER";
+	}
+	case ENCODE1:
+	{
+		return L"BYTE";
+	}
+	case ENCODE2:
+	{
+		return L"WORD";
+	}
+	case ENCODE4:
+	{
+		return L"DWORD";
+	}
+	case ENCODE8:
+	{
+		return L"QWORD";
+	}
+	case ENCODESTR:
+	{
+		return L"Str(" + std::to_wstring(pf.size - sizeof(WORD)) + L")";
+	}
+	case ENCODEBUFFER:
+	{
+		return L"Buffer(" + std::to_wstring(pf.size - sizeof(WORD)) + L")";
+	}
+	case DECODEHEADER:
+	{
+		return L"Header";
+	}
+	case DECODE1:
+	{
+		return L"BYTE";
+	}
+	case DECODE2:
+	{
+		return L"WORD";
+	}
+	case DECODE4:
+	{
+		return L"DWORD";
+	}
+	case DECODE8:
+	{
+		return L"QWORD";
+	}
+	case DECODESTR:
+	{
+		return L"Str(" + std::to_wstring(pf.size - sizeof(WORD)) + L")";
+	}
+	case DECODEBUFFER:
+	{
+		return L"Buffer(" + std::to_wstring(pf.size - sizeof(WORD)) + L")";
+	}
+	// エラー処理
+	case NOTUSED: {
+		return L"Not Used(" + std::to_wstring(pf.size) + L")";
+	}
+	case UNKNOWNDATA: {
+		return L"Unknown(" + std::to_wstring(pf.size) + L")";
+	}
+	case WHEREFROM: {
+		return L"Not Encoded(" + std::to_wstring(pf.size) + L")";
+	}
+	}
+	return L"Error";
+}
+
+// ShiftJIS to UTF16
+bool ShiftJIStoUTF8(std::string sjis, std::wstring &utf16) {
+	try {
+		// UTF16へ変換する際の必要なバイト数を取得
+		int len = MultiByteToWideChar(CP_ACP, 0, sjis.c_str(), -1, 0, 0);
+		if (!len) {
+			return false;
+		}
+
+		// UTF16へ変換
+		std::vector<BYTE> b((len + 1) * sizeof(WORD));
+		if (!MultiByteToWideChar(CP_ACP, 0, sjis.c_str(), -1, (WCHAR *)&b[0], len)) {
+			return false;
+		}
+
+		utf16 = std::wstring((WCHAR *)&b[0]);
+		return true;
+	}
+	catch (...) {
+		return false;
+	}
+
+	return true;
+}
+
+// バイト配列からShiftJIS文字列を取得
+bool BYTEtoShiftJIS(BYTE *text, int len, std::string &sjis) {
+	try {
+		std::vector<BYTE> b(len + 1);
+		for (size_t i = 0; i < len; i++) {
+			b[i] = text[i];
+		}
+		sjis = std::string((char *)&b[0]);
+	}
+	catch (...) {
+		return false;
+	}
+	return true;
+}
+
+std::wstring GetFormatData(PacketData &pd, PacketFormat &pf) {
+	switch (pf.type) {
+	case ENCODEHEADER:
+	case DECODEHEADER:
+	{
+		return L"@" + WORDtoString(*(WORD *)&pd.packet[pf.pos]);
+	}
+	case ENCODE1:
+	case DECODE1:
+	{
+		return BYTEtoString(pd.packet[pf.pos]);
+	}
+	case ENCODE2:
+	case DECODE2:
+	{
+		return WORDtoString(*(WORD *)&pd.packet[pf.pos]);
+	}
+	case ENCODE4:
+	case DECODE4:
+	{
+		return DWORDtoString(*(DWORD *)&pd.packet[pf.pos]);
+	}
+	case ENCODE8:
+	case DECODE8:
+	{
+		return DWORDtoString(*(DWORD *)&pd.packet[pf.pos + sizeof(DWORD)]) + DWORDtoString(*(DWORD *)&pd.packet[pf.pos]);
+	}
+	case ENCODESTR:
+	case DECODESTR:
+	{
+		size_t len = *(WORD *)&pd.packet[pf.pos];
+		std::string sjis;
+		std::wstring utf16;
+		if (BYTEtoShiftJIS(&pd.packet[pf.pos + sizeof(WORD)], len, sjis) && ShiftJIStoUTF8(sjis, utf16)) {
+			return L"\"" + utf16 + L"\"";
+		}
+		return DatatoString(&pd.packet[pf.pos], pf.size + sizeof(WORD));
+	}
+	case ENCODEBUFFER:
+	case DECODEBUFFER:
+	{
+		return DatatoString(&pd.packet[pf.pos], pf.size);
+	}
+	// エラー処理
+	case NOTUSED:
+	case UNKNOWNDATA:
+	case WHEREFROM: {
+		return DatatoString(&pd.packet[pf.pos], pf.size, true);
+	}
+	default: {
+		break;
+	}
+	}
+	return L"Error";
+}
+
+bool GetIntData(PacketData &pd, PacketFormat &pf, int &val) {
+	switch (pf.type) {
+	case ENCODE1:
+	case DECODE1:
+	{
+		val = (int)pd.packet[pf.pos];
+		return true;
+	}
+	case ENCODE2:
+	case DECODE2:
+	{
+		val = (int)*(WORD *)&pd.packet[pf.pos];
+		return true;
+	}
+	case ENCODE4:
+	case DECODE4:
+	{
+		val = (int)(*(DWORD *)&pd.packet[pf.pos]);
+		return true;
+	}
+	default: {
+		break;
+	}
+	}
+	return false;
+}
+
 // format info
 bool SetExtraInfo(Alice &a, std::vector<PacketData>& vpd, DWORD id) {
+	bool show_return = a.CheckBoxStatus(CHECK_SHOW_RETURN);
+	bool show_int = a.CheckBoxStatus(CHECK_SHOW_INTDATA);
+	bool show_raw = a.CheckBoxStatus(CHECK_SHOW_RAWDATA);
+
 	for (auto &pd : vpd) {
 		if (pd.id == id) {
 			std::wstring wText;
@@ -41,8 +239,43 @@ bool SetExtraInfo(Alice &a, std::vector<PacketData>& vpd, DWORD id) {
 			wText += L"Length = " + std::to_wstring(pd.packet.size()) + L"\r\n";
 			wText += L"\r\n";
 			wText += L"[Format]\r\n";
+			int count = 0;
+			size_t prev_pos = 0;
 			for (auto &pf : pd.format) {
+				if (pf.pos < prev_pos) {
+					wText += L"Something broken\r\n";
+					break;
+				}
+				prev_pos = pf.pos;
+
+				wText += L"<" + std::to_wstring(count) + L">\r\n";
+				wText += L"Position = +" + std::to_wstring(pf.pos) + L"\r\n";
+				wText += L"Type = " + GetFormatType(pf) + L"\r\n";
+				// Return Address
+				if (show_return) {
+					wText += L"Return = " + QWORDtoString(pf.addr) + L"\r\n";
+				}
+				wText += L"Data = " + GetFormatData(pd, pf) + L"\r\n";
+				// 整数値
+				if (show_int) {
+					int val = 0;
+					if (GetIntData(pd, pf, val)) {
+						wText += L"Int = " + std::to_wstring(val) + L"\r\n";
+					}
+				}
+				if (show_raw) {
+					wText += L"Raw = " + DatatoString(&pd.packet[pf.pos], pf.size, true) + L"\r\n";
+				}
+				count++;
 			}
+
+			// オリジナルデータ
+			if (show_raw) {
+				wText += L"\r\n";
+				wText += L"[Raw]\r\n";
+				wText += L"Raw = " + DatatoString(&pd.packet[0], pd.packet.size(), true) + L"\r\n";
+			}
+
 			a.SetText(EDIT_EXTRA, wText);
 			return true;
 		}
@@ -62,9 +295,15 @@ bool UpdateLogger(PacketEditorMessage &pem, bool bBlock) {
 
 	std::wstring wType;
 	if (pem.header == SENDPACKET) {
+		if (!a.CheckBoxStatus(CHECK_SEND)) {
+			return false;
+		}
 		wType = L"Send";
 	}
 	else if (pem.header == RECVPACKET) {
+		if (!a.CheckBoxStatus(CHECK_RECV)) {
+			return false;
+		}
 		wType = L"Recv";
 	}
 
@@ -111,9 +350,14 @@ bool OnCreate(Alice &a) {
 	a.ListView_AddHeader(LISTVIEW_LOGGER, L"ID", 50);
 	a.ListView_AddHeader(LISTVIEW_LOGGER, L"Length", 50);
 	a.ListView_AddHeader(LISTVIEW_LOGGER, L"Packet", (PE_WIDTH - 180));
-	a.TextArea(EDIT_EXTRA, 3, (PE_HEIGHT / 2), ((PE_WIDTH / 2) - 6), (PE_HEIGHT / 2 - 6));
+	a.TextArea(EDIT_EXTRA, 3, (PE_HEIGHT / 2 + 20), ((PE_WIDTH / 2) - 6), (PE_HEIGHT / 2 - 6 - 20));
 	a.SetText(EDIT_EXTRA, L"Format View");
 	a.ReadOnly(EDIT_EXTRA);
+	a.CheckBox(CHECK_SHOW_RETURN, L"Return", 3, (PE_HEIGHT / 2), BST_CHECKED);
+	a.CheckBox(CHECK_SHOW_RAWDATA, L"Raw", 80, (PE_HEIGHT / 2), BST_UNCHECKED);
+	a.CheckBox(CHECK_SHOW_INTDATA, L"Int", 130, (PE_HEIGHT / 2), BST_UNCHECKED);
+
+
 	a.CheckBox(CHECK_SEND, L"Send", (PE_WIDTH - 100), (PE_HEIGHT / 2 + 10), BST_CHECKED);
 	a.CheckBox(CHECK_RECV, L"Recv", (PE_WIDTH - 50), (PE_HEIGHT / 2 + 10), BST_CHECKED);
 	a.Button(BUTTON_CLEAR, L"Clear", (PE_WIDTH - 150), (PE_HEIGHT / 2 + 10));
